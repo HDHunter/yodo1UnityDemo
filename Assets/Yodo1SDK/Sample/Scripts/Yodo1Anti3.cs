0bc77d879e75073efbe803f80269aa3dc8662335
HunterZhang <zhangjianqiu007@126.com>
2021-04-28 18:31:05 +0800

    anti3.0 add.

--- Assets/Yodo1SDK/Sample/Scripts/Yodo1Anti3.cs
+++ Assets/Yodo1SDK/Sample/Scripts/Yodo1Anti3.cs
@@ -1,93 +1,119 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Yodo1.AntiAddiction;
 using Yodo1Unity;
 using Yodo1JSON;
 
 public class Yodo1Anti3 : MonoBehaviour
 {
-    private static Yodo1U3dUser gameUser = new Yodo1U3dUser();
-
-    public static Yodo1U3dUser GetGameUser()
+    void Start()
     {
-        return gameUser;
+        // Debug.Log(Yodo1U3dConstants.LOG_TAG + " SetInitCallBack result:" + result + "   content:" + content);
     }
 
     // Use this for initialization
-    void Start()
+    void initAniti3()
     {
-        Yodo1U3dAccount.SetLoginDelegate(LoginDelegate);
-        Yodo1U3dAccount.SetSwitchAccountDelegate(SwitchAccountDelegate);
-        Yodo1U3dAccount.SetLogoutDelegate(LogoutDelegate);
-        Yodo1U3dAccount.SetRegistDelegate(RegistDelegate);
-
-        if (Yodo1U3dUtils.IsChineseMainland()) {
-
-            Debug.Log(Yodo1U3dConstants.LOG_TAG + "中国大陆用户");
-        }
-        else
+        // 设置sdk初始化结果回调，请在收到初始化成功的回调后再去调用其他接口。
+        Yodo1U3dAntiAddiction.SetInitCallBack((bool result, string content) =>
         {
-            Debug.Log(Yodo1U3dConstants.LOG_TAG + "不是中国大陆用户");
-        }
-
-    }
-
-    void LoginDelegate(Yodo1U3dConstants.AccountEvent accountEvent, Yodo1U3dUser user)
-    {
-        if (accountEvent == Yodo1U3dConstants.AccountEvent.Success)
-        {
-            Debug.Log(Yodo1U3dConstants.LOG_TAG + "login success, " + user.toJson());
-            if (user != null)
+            Debug.Log(Yodo1U3dConstants.LOG_TAG + " SetInitCallBack result:" + result + "   content:" + content);
+            if (result)
             {
-                gameUser = user;
-                Yodo1U3dImpubicProtect.IndentifyUser(gameUser.UserId, IndentifyUserDelegate);
+                // 初始化成功，可以继续执行游戏逻辑
+                Debug.Log(Yodo1U3dConstants.LOG_TAG + " SetInitCallBack 初始化成功，可以继续执行游戏逻辑");
             }
             else
             {
-                Yodo1U3dUtils.ShowAlert("Warning", "登录失败...", "Ok");
+                // 初始化失败，请结束游戏 
+                Debug.Log(Yodo1U3dConstants.LOG_TAG + " SetInitCallBack 初始化失败，请结束游戏");
             }
-        }
-        else if (accountEvent == Yodo1U3dConstants.AccountEvent.Fail)
-        {
-            Debug.Log(Yodo1U3dConstants.LOG_TAG + "login failed");
-        }
-    }
-
-    void SwitchAccountDelegate(Yodo1U3dConstants.AccountEvent accountEvent, Yodo1U3dUser user)
-    {
-        if (accountEvent == Yodo1U3dConstants.AccountEvent.Success)
-        {
-            Debug.Log(Yodo1U3dConstants.LOG_TAG + "switchAccount success, " + user.toJson());
-            if (user != null)
+        });
+        // 设置接收玩家到达游戏时间上限的事件监听器。当未成年人或游客到达时间上限(或即将到达时)，SDK会通过此回调来通知开发者
+        Yodo1U3dAntiAddiction.SetTimeLimitNotifyCallBack(
+            (Yodo1U3dEventAction action, int eventCode, string title, string content) =>
             {
-                gameUser = user;
+                Debug.Log(Yodo1U3dConstants.LOG_TAG + " SetTimeLimitNotifyCallBack eventcode:" + eventCode + " title:" +
+                          title + " content:" + content);
+                if (action == Yodo1U3dEventAction.ResumeGame)
+                {
+                    // 当状态值为ResumeGame时，代表仅需给玩家展示即将到时的提醒，此时不需要结束游戏。
+                    Dialog.ShowMsgDialog(title, content);
+                    Debug.Log(Yodo1U3dConstants.LOG_TAG + " Yodo1U3dEventAction.ResumeGame");
+                }
+                else if (action == Yodo1U3dEventAction.EndGame)
+                {
+                    // 当状态为EndGame时，代表玩家已到达最大游戏时长，必须结束游戏。
+                    Dialog.ShowMsgDialog(title, content, true, () => { Application.Quit(); });
+                    Debug.Log(Yodo1U3dConstants.LOG_TAG + " Yodo1U3dEventAction.EndGame");
+                }
+            });
+        // 设置玩家掉线监听器。当玩家网络连接有问题，防沉迷系统不能正确统计时，SDK会通过此回调通知开发者
+        Yodo1U3dAntiAddiction.SetPlayerDisconnectionCallBack((string title, string content) =>
+        {
+            // 此时不可继续游戏。
+            // 开发者应该主动调用上线接口尝试上线，直到返回成功才可让玩家继续游戏。
+            Online();
+            Debug.Log(Yodo1U3dConstants.LOG_TAG + " SetPlayerDisconnectionCallBack,title:" + title + " content:" +
+                      content);
+        });
+        Yodo1U3dAntiAddiction.Init();
+        Debug.Log(Yodo1U3dConstants.LOG_TAG + " Yodo1U3dAntiAddiction.Init");
+    }
+
+    void Online()
+    {
+        Debug.Log(Yodo1U3dConstants.LOG_TAG + " Online.");
+        Yodo1U3dAntiAddiction.Online((bool result, string content) =>
+        {
+            Debug.Log(Yodo1U3dConstants.LOG_TAG + " Online.result:" + result + "  content:" + content);
+            if (result)
+            {
+                // 上线成功，开始游戏
+                Debug.Log(Yodo1U3dConstants.LOG_TAG + " Online.上线成功，开始游戏");
             }
-        }
-        else if (accountEvent == Yodo1U3dConstants.AccountEvent.Fail)
-        {
-            Debug.Log(Yodo1U3dConstants.LOG_TAG + "switchAccount failed");
-        }
+        });
     }
 
-    void LogoutDelegate(Yodo1U3dConstants.AccountEvent accountEvent)
+    void offline()
     {
-        if (accountEvent == Yodo1U3dConstants.AccountEvent.Success)
+        Debug.Log(Yodo1U3dConstants.LOG_TAG + " Offline.");
+        Yodo1U3dAntiAddiction.Offline((bool result, string content) =>
         {
-            Debug.Log(Yodo1U3dConstants.LOG_TAG + "Logout success");
-        }
-        else if (accountEvent == Yodo1U3dConstants.AccountEvent.Fail)
-        {
-            Debug.Log(Yodo1U3dConstants.LOG_TAG + "Logout failed");
-        }
+            Debug.Log(Yodo1U3dConstants.LOG_TAG + " Offline.result:" + result + "  content:" + content);
+            if (result)
+            {
+                // 下线成功，退出游戏
+                Debug.Log(Yodo1U3dConstants.LOG_TAG + " Offline.下线成功，退出游戏");
+            }
+        });
     }
 
-    void RegistDelegate(Yodo1U3dConstants.AccountEvent accountEvent)
+    public void readyEnterGame(string accountId)
     {
-        if (accountEvent == Yodo1U3dConstants.AccountEvent.Success)
+        Debug.Log(Yodo1U3dConstants.LOG_TAG + " VerifyCertificationInfo,accountId:" + accountId);
+        // 调用实名认证接口
+        Yodo1U3dAntiAddiction.VerifyCertificationInfo(accountId, (Yodo1U3dEventAction eventAction) =>
         {
-            Debug.Log(Yodo1U3dConstants.LOG_TAG + "Regsit success");
-        }
+            Debug.Log(Yodo1U3dConstants.LOG_TAG + " VerifyCertificationInfo,accountId:" + accountId);
+            if (eventAction == Yodo1U3dEventAction.ResumeGame)
+            {
+                // 实名认证成功后可查询玩家是否为游客模式
+                // bool isGuestUser = Yodo1U3dAntiIndulged.IsGuestUser();
+                // 此时可继续游戏
+                Online();
+                Debug.Log(Yodo1U3dConstants.LOG_TAG + " VerifyCertificationInfo,实名认证成功后可查询玩家是否为游客模式");
+            }
+            else if (eventAction == Yodo1U3dEventAction.EndGame)
+            {
+                // 实名认证失败后不可继续进入游戏
+                Dialog.ShowMsgDialog("提示", "实名认证失败", true, () => { Application.Quit(); });
+                Debug.Log(Yodo1U3dConstants.LOG_TAG + " VerifyCertificationInfo,实名认证失败后不可继续进入游戏");
+            }
+        });
     }
 
     // Update is called once per frame
@@ -115,7 +141,6 @@ public class Yodo1Anti3 : MonoBehaviour
         }
 
 #if UNITY_IPHONE
-
         loginExtr = GUI.TextField(new Rect(btn_x, btn_startY, btn_w, btn_h), loginExtr);
 
         if (GUI.Button(new Rect(btn_x, btn_startY * 2 + btn_h, btn_w, btn_h), "iOS 防沉迷实名初始化"))
@@ -150,207 +175,63 @@ public class Yodo1Anti3 : MonoBehaviour
 
 #else
 
-        loginExtr = GUI.TextField(new Rect(btn_x, btn_startY, btn_w, btn_h), loginExtr);
-
-        if (GUI.Button(new Rect(btn_x, btn_startY * 2 + btn_h, btn_w, btn_h), "渠道登录-loginExtr"))
+        if (GUI.Button(new Rect(btn_x, btn_startY, btn_w, btn_h), "初始化防沉迷系统"))
         {
-            Yodo1U3dAccount.Login(Yodo1U3dConstants.LoginType.Channel, loginExtr);
+            initAniti3();
         }
 
-        if (GUI.Button(new Rect(btn_x, btn_startY * 3 + btn_h * 2, btn_w, btn_h), "设备登录-loginExtr"))
+        if (GUI.Button(new Rect(btn_x, btn_startY * 2 + btn_h, btn_w, btn_h), "实名认证"))
         {
-            Yodo1U3dAccount.Login(Yodo1U3dConstants.LoginType.Device, loginExtr);
+            readyEnterGame(Yodo1Account.GetGameUser().UserId);
         }
 
-        if (GUI.Button(new Rect(btn_x, btn_startY * 4 + btn_h * 3, btn_w, btn_h), "提交用户信息"))
+        if (GUI.Button(new Rect(btn_x, btn_startY * 4 + btn_h * 3, btn_w, btn_h), "上线"))
         {
-            if (gameUser != null)
+            Online();
+        }
+
+        if (GUI.Button(new Rect(btn_x, btn_startY * 5 + btn_h * 4, btn_w, btn_h), "下线"))
+        {
+            offline();
+        }
+
+        loginExtr = GUI.TextField(new Rect(btn_x, btn_startY * 6 + btn_h * 5, btn_w, btn_h), loginExtr);
+
+
+        if (GUI.Button(new Rect(btn_x, btn_startY * 7 + btn_h * 6, btn_w, btn_h), "支付验证"))
+        {
+            // 接口需要传入商品价格(单位为分)，以及货币符号(CNY)。
+            Yodo1U3dAntiAddiction.VerifyPurchase(Double.Parse(loginExtr), "CNY", (bool isAllow, string content) =>
             {
-                Yodo1U3dAccount.SubmitUser(gameUser);
-            }
+                Debug.Log(Yodo1U3dConstants.LOG_TAG + " VerifyPurchase,isAllow:" + isAllow + "  content:" + content);
+                if (isAllow)
+                {
+                    // 可以继续购买，请继续支付流程
+                    // Purchase(price);
+                }
+                else
+                {
+                    // 已达到上限，不可继续购买。此时请用回调中返回的content提示玩家
+                    Dialog.ShowMsgDialog("提示", content);
+                }
+            });
         }
 
-        if (GUI.Button(new Rect(btn_x, btn_startY * 5 + btn_h * 4, btn_w, btn_h), "切换账号-loginExtr"))
+        if (GUI.Button(new Rect(btn_x, btn_startY * 8 + btn_h * 7, btn_w, btn_h), "上传商品信息"))
         {
-            Yodo1U3dAccount.SwitchAccount(Yodo1U3dConstants.LoginType.Channel, loginExtr);
+            string productId = "1111111"; // 商品ID
+            Yodo1U3dProductType type = Yodo1U3dProductType.Consumables; // 商品类型，消耗品/非消耗品
+            double priceCent = Double.Parse(loginExtr); // 价格，单位为分
+            string currency = "CNY"; // 货币符号，CNY
+            string orderId = "2021年04月28日18:30:01-5.6.0"; // 订单号
+            Yodo1U3dAntiAddiction.ReportProductReceipt(productId, type, priceCent, currency, orderId);
         }
 
-        if (GUI.Button(new Rect(btn_x, btn_startY * 6 + btn_h * 5, btn_w, btn_h), "登出"))
-        {
-            Yodo1U3dAccount.Logout();
-        }
-
-        if (GUI.Button(new Rect(btn_x, btn_startY * 7 + btn_h * 6, btn_w, btn_h), "查询游戏剩余时长"))
-        {
-            if (gameUser.Age > 0)
-            {
-                QueryRemainingTime();
-            }
-            else
-            {
-                Yodo1U3dUtils.ShowAlert("Warning", "无效年龄，无需处理游戏剩余时长", "Ok");
-            }
-        }
-        if (GUI.Button(new Rect(btn_x, btn_startY * 8 + btn_h * 7, btn_w, btn_h), "查询防沉迷规则"))
-        {
-            if (gameUser.Age > 0)
-            {
-                QueryImpubicProtectConfig();
-            }
-            else
-            {
-                Yodo1U3dUtils.ShowAlert("Warning", "无效年龄，无法查询防沉迷规则", "Ok");
-            }
-        }
-
-        if (GUI.Button(new Rect(btn_x, btn_startY * 9 + btn_h * 8, btn_w, btn_h), "查询用户协议和隐私信息"))
-        {
-            QueryTermsAndPrivacy();
-        }
-               
-        if (GUI.Button(new Rect(btn_x, btn_startY * 10 + btn_h * 9, btn_w, btn_h), "打开排行榜"))
-        {
-            Yodo1U3dLeaderboard.leaderboardsOpen();
-        }
-
-
-        if (GUI.Button(new Rect(btn_x, btn_startY * 11 + btn_h * 10, btn_w, btn_h), "打开成就"))
-        {
-            Yodo1U3dAchievement.achievementsOpen();
-        }
-
-        if (GUI.Button(new Rect(btn_x, btn_startY * 12 + btn_h * 11, btn_w, btn_h), "返回"))
+        if (GUI.Button(new Rect(btn_x, btn_startY * 9 + btn_h * 8, btn_w, btn_h), "返回"))
         {
             SceneManager.LoadScene("Yodo1Demo");
         }
 
 #endif
     }
-
-    // Impubic Protect
-    void IndentifyUserDelegate(int reslutCode, Yodo1U3dImpubicProtect.Indentify indentify, Yodo1U3dImpubicProtect.VerifiedStatus verifiedStatus, int age)
-    {
-        Debug.Log(string.Format(Yodo1U3dConstants.LOG_TAG + "IndentifyUserDelegate, reslutCode:{0}, indentify:{1}, verifiedStatus:{2},age:{3}", reslutCode, indentify.ToString(), verifiedStatus.ToString(), age));
-
-        if (reslutCode == Yodo1U3dConstants.RUSULT_CODE_SUCCESS)
-        {   //实名认证成功
-            Yodo1U3dSDK.SetTagForUnderAgeOfConsent(true);
-
-            ContinueGame(age);
-        }
-        else if (reslutCode == Yodo1U3dConstants.RUSULT_CODE_CANCEL)
-        {   //游客试玩
-            ContinueGame(1);
-        }
-        else
-        {
-            if (verifiedStatus == Yodo1U3dImpubicProtect.VerifiedStatus.StopGame)
-            {//不可继续游戏
-                Debug.Log(Yodo1U3dConstants.LOG_TAG + "需要停止游戏...");
-                Yodo1U3dUtils.ShowAlert("Warning", "认证失败，退出游戏...", "Ok", "", "", this, QuitGame);
-            }
-            else if (verifiedStatus == Yodo1U3dImpubicProtect.VerifiedStatus.ResumeGame)
-            {//继续游戏
-                Debug.Log(Yodo1U3dConstants.LOG_TAG + "继续游戏...");
-                ContinueGame(-1);
-            }
-            else if (verifiedStatus == Yodo1U3dImpubicProtect.VerifiedStatus.ForeignIP)
-            {//继续游戏 国外IP
-                Debug.Log(Yodo1U3dConstants.LOG_TAG + "继续游戏...");
-                ContinueGame(-1);
-            }
-        }
-    }
-
-    void ContinueGame(int age)
-    {
-        gameUser.Age = age;
-        Yodo1U3dAccount.SubmitUser(gameUser);
-
-        //创建防沉迷系统
-        Yodo1U3dImpubicProtect.CreateImpubicProtectSystem(age, (int resultCode, string msg) =>
-        {
-            if (resultCode == Yodo1U3dConstants.RUSULT_CODE_SUCCESS)
-            {
-                // 打开玩家时长限制计时器
-                Yodo1U3dImpubicProtect.StartPlaytimeKeeper(ConsumePlaytimeDelegate, PlaytimeOverDelegate, RemainTimeTipsDelegate);
-                Yodo1U3dImpubicProtect.SetPlaytimeNotifyTime(60);
-
-                QueryImpubicProtectConfig();
-            }
-            else
-            {
-                // 失败了整个防沉迷系统的接口都会失效
-            }
-        });
-    }
-
-    void RemainTimeTipsDelegate(string title, string content)
-    {
-         Debug.Log(string.Format(Yodo1U3dConstants.LOG_TAG + "RemainTimeTipsDelegate, title:{0}, content:{1}", title, content));
-    
-    }
-
-    void ConsumePlaytimeDelegate(long playedTime, long remainingTime)
-    {
-        Debug.Log(string.Format(Yodo1U3dConstants.LOG_TAG + "ConsumePlaytimeDelegate, playedTime:{0}, remainingTime:{1}", playedTime, remainingTime));
-    }
-
-    void PlaytimeOverDelegate(int resultCode, string msg, long playedTime)
-    {
-        Debug.Log(string.Format(Yodo1U3dConstants.LOG_TAG + "PlaytimeOverDelegate, resultCode:{0}, msg:{1}, playedTime: {2}", resultCode, msg, playedTime));
-        //游戏时长为0, 提示玩家
-        Yodo1U3dUtils.ShowAlert("Warning", string.Format("ResultCode:{0}\nErrorMessage:{1}", resultCode, msg), "Ok", "", "", this, QuitGame);
-
-    }
-
-    public void QueryRemainingTime()
-    {
-        Yodo1U3dImpubicProtect.QueryPlayerRemainingTime((int resultCode, string msg, double remainingTime) =>
-        {
-            Debug.Log(string.Format(Yodo1U3dConstants.LOG_TAG + "QueryPlayerRemainingTime, resultCode:{0}, msg:{1}, remainingTime:{2}", resultCode, msg, remainingTime));
-        });
-    }
-
-    public void QueryImpubicProtectConfig()
-    {
-        Yodo1U3dImpubicProtect.QueryImpubicProtectConfig((int resultCode, string msg, Yodo1U3dImpubicProtectConfig config) =>
-        {
-            Debug.Log(string.Format(Yodo1U3dConstants.LOG_TAG + "QueryImpubicProtectConfig, resultCode:{0}, msg:{1}, config:{2}", resultCode, msg, config.GetJsonString()));
-        });
-    }
-
-    public void QueryTermsAndPrivacy()
-    {
-        Yodo1U3dSDK.QueryUserAgreementAndPrivacyInfo((bool isUpdate, int limitAge, string termsUrl, string privacyUrl) =>
-        {
-            Debug.Log(string.Format(Yodo1U3dConstants.LOG_TAG + "QueryUserAgreementAndPrivacyInfo, isUpdate:{0}, limitAge:{1}, termsUrl:{2}, privacyUrl:{3}", isUpdate, limitAge, termsUrl, privacyUrl));
-        });
-    }
-
-    public void QueryPaymentAmount()
-    {
-        int price = int.Parse(loginExtr);
-        Debug.Log(string.Format(Yodo1U3dConstants.LOG_TAG + "price:{0}", price));
-
-        Yodo1U3dImpubicProtect.VerifyPaymentAmount(price, (int resultCode, string msg) =>
-        {
-            Debug.Log(string.Format(Yodo1U3dConstants.LOG_TAG + "VerifyPaymentAmount, resultCode:{0}, msg:{1}", resultCode, msg));
-        });
-    }
-
-    public void QueryPaymentConst()
-    {
-        Yodo1U3dImpubicProtect.QueryPlayerRemainingCost((int resultCode, string msg, double cost) =>
-        {
-            Debug.Log(string.Format(Yodo1U3dConstants.LOG_TAG + "QueryPlayerRemainingCost, resultCode:{0}, msg:{1},cost:{2}", resultCode, msg,cost));
-        });
-    }
-
-
-    public void QuitGame(string msg)
-    {
-        Application.Quit();
-    }
-
 }
